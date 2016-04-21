b3a2e7a56c4b62874abc77c66d066d3cbefc5dc0
druan10 <druan10@gmail.com>
2016-04-21 16:46:56 -0700

    Added files via upload
    My code 4-21-16 4:46 pm

--- project.py
+++ project.py
@@ -1,19 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 20 12:26:49 2016
+Created on Wed Apr 20 12:06:25 2016
 
-@author: rishijavia
+@author: David Ruan
 """
-
 
 from bs4 import BeautifulSoup
 import requests
-url = "hltv.org"
 
-r = requests.get("http://" +url)
-
+hltvUrl = "www.hltv.org/results/"
+r  = requests.get("http://" +hltvUrl)
 data = r.text
-
 soup = BeautifulSoup(data)
+results=soup.find_all("div", {"class": "matchListBox"},limit=50)
+#print(soup.prettify())
 
-print(soup.prettify())
+print('******************')
+res=results[0]
+print(res.contents)
+
+print(len(res))
+
+for i in res:
+    print(i)
+
+"""
+    #find team ranking on hltv
+def getTeamRank(team):
+    r  = requests.get("http://www.hltv.org/ranking/teams/")
+    data = r.text
+    soup = BeautifulSoup(data)
+    div=soup.div
+    logo = div.attrs['ranking-logo']
+    
+getTeamRank('Natus Vincere')
+
+"""
