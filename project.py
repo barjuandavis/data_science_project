6488fd4360c2e2ff8f5e00bc0b11bdc28d04c33d
druan10 <druan10@gmail.com>
2016-04-21 16:52:38 -0700

    Added files via upload

--- project.py
+++ project.py
@@ -9,22 +9,28 @@ from bs4 import BeautifulSoup
 import requests
 
 hltvUrl = "www.hltv.org/results/"
+gosuUrl = ""
 r  = requests.get("http://" +hltvUrl)
 data = r.text
 soup = BeautifulSoup(data)
+print(soup.prettify())
+
+
 results=soup.find_all("div", {"class": "matchListBox"},limit=50)
-#print(soup.prettify())
+print(soup.prettify())
 
 print('******************')
 res=results[0]
-print(res.contents)
+print(res)
 
+
+"""
 print(len(res))
 
 for i in res:
     print(i)
 
-"""
+
     #find team ranking on hltv
 def getTeamRank(team):
     r  = requests.get("http://www.hltv.org/ranking/teams/")
