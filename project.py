b598360c4c00102fa1b360b2c06d538c9fd8a4a9
Rishi <rishijavia@lclark.edu>
2016-04-21 16:53:42 -0700

    bullshit code

--- project.py
+++ project.py
@@ -8,12 +8,28 @@ Created on Wed Apr 20 12:26:49 2016
 
 from bs4 import BeautifulSoup
 import requests
-url = "hltv.org"
+import urllib
+url = "hltv.org/?pageid=179&teamid=6290&statsfilter=9"
 
 r = requests.get("http://" +url)
 
 data = r.text
 
-soup = BeautifulSoup(data)
+soup = BeautifulSoup(data, "lxml")
 
-print(soup.prettify())
+#print(soup.prettify())
+
+def match_class(target):                                                        
+    def do_match(tag):                                                          
+        classes = tag.get('class', [])                                          
+        return all(c in classes for c in target)                                
+    return do_match    
+
+soup = BeautifulSoup(html)  
+
+matches = soup.find_all(match_class(["covSmallHeadline"]))
+print(matches)
+for m in matches:
+    print (m)
+
+#covSmallHeadline
