5f5974fa2a5882cc8445164b73efb49946f5076a
Rishi <rishijavia@lclark.edu>
2016-04-21 17:16:55 -0700

    Print class and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # data_science_project
 CS 298 Data Science Project about CS GO data
+
+Teams: fnatic and NIP
+Variables: map, %win on that map, average player rating
--- project.py
+++ project.py
@@ -7,12 +7,12 @@ Created on Wed Apr 20 12:06:25 2016
 from bs4 import BeautifulSoup
 import requests
 
-hltvUrl = "www.hltv.org/results/"
+hltvUrl = "www.hltv.org/?pageid=179&teamid=4991"
 r  = requests.get("http://" +hltvUrl)
 data = r.text
-soup = BeautifulSoup(data)
+soup = BeautifulSoup(data, "lxml")
 results=soup.find_all("div", {"class": "matchListBox"},limit=50)
-#print(soup.prettify())
+print(soup.prettify())
 
 print('******************')
 res=results[0]
@@ -22,16 +22,7 @@ print(len(res))
 
 for i in res:
     print(i)
-
-"""
-    #find team ranking on hltv
-def getTeamRank(team):
-    r  = requests.get("http://www.hltv.org/ranking/teams/")
-    data = r.text
-    soup = BeautifulSoup(data)
-    div=soup.div
-    logo = div.attrs['ranking-logo']
     
-getTeamRank('Natus Vincere')
-
-"""
+#mydivs = soup.findAll("div", { "class" : "matchScoreCell" })
+#
+#print(mydivs)
