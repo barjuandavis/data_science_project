cbb8e91d2a30a6b12ea7b05e25047e62bc25d0c7
druan10 <druan10@gmail.com>
2016-04-22 20:37:09 -0700

    Added files via upload
    Basic HLTV Scraping and exporting to csv file.
    
    CSV contains these values currently
    "Winning Team, Losing Team, Winning Score, Losing Score, Map Played"
    
    The call to scrape at the bottom of the code creates the csv file using 100 results pages of hltv

--- project.py
+++ project.py
@@ -4,11 +4,11 @@ Created on Wed Apr 20 12:06:25 2016
 
 @author: David Ruan
 """
-
 from bs4 import BeautifulSoup
 import requests
 
 def extractDataIntoCondensedList(match):
+    line=''
     #match is a list
     thisMap=(match[2].replace(' - ', ' ').split(' '))[0]
     firstTeam=(match[2].replace(' - ', ' ').split(' '))[1]
@@ -17,52 +17,35 @@ def extractDataIntoCondensedList(match):
     secondScore=int((match[7].replace(' - ', ' ').split(' '))[1])
     #do not include anything other than best of 1's
     if (firstScore+secondScore>=16):
-        print()
-        print(match)
-        print(thisMap)
-        print(firstTeam)
-        print(secondTeam)
-        print(firstScore)
-        print(secondScore)
         if (firstScore>secondScore):
-            print(firstTeam+' beat ' + secondTeam + ' ' + str(firstScore) + '-' + str(secondScore) + ' on '+ thisMap)
+            #print(firstTeam+' beat ' + secondTeam + ' ' + str(firstScore) + '-' + str(secondScore) +' on '+thisMap)
             #convert into csv format
-            csv=str(firstTeam + ',' + secondTeam + ',' + str(firstScore) + ',' + str(secondScore) + ',' + thisMap)
+            line+=str('\n' + firstTeam + ',' + secondTeam + ',' + str(firstScore) + ',' + str(secondScore) + ',' + thisMap)
         else:
-            print(secondTeam+' beat ' + firstTeam + ' ' + str(secondScore) + '-' + str(firstScore) + ' on '+ thisMap)
-            csv=str(secondTeam + ',' + firstTeam + ',' + str(secondScore) + ',' + str(firstScore) + ',' + thisMap)
+            #print(secondTeam+' beat ' + firstTeam + ' ' + str(secondScore) + '-' + str(firstScore) +' on '+thisMap)
+            line+=str('\n' + secondTeam + ',' + firstTeam + ',' + str(secondScore) + ',' + str(firstScore) + ',' + thisMap)
             #convert to csv format
-        print(csv)
-        
-
-hltvUrl = "http://www.hltv.org/results/"
-gosuUrl = ""
-fnaticStatsUrl = "http://www.hltv.org/?pageid=179&teamid=4991"
-r  = requests.get(hltvUrl)
-data = r.text
-soup = BeautifulSoup(data)
-#print(soup.prettify())
-print()
-results=soup.find_all("div", {"class": "matchListBox"},limit=50)
-for i in results:
-    #to search for a specific team, use if 'teamname' in i.text:
-    result=(str(i.text).splitlines())
-    #gather data
-    extractDataIntoCondensedList(result)
+    return line
     
-"""
-for i in res:
-    print(i)
+#Only run this if you want new data (Will overwrite the previoius csv file)
+def scrape(pages):
+    print("Scraping. Please be patient")
+    csv=''
+    for i in range(pages):
+        hltvUrl = "http://www.hltv.org/results/"
+        if i==0:
+            csvFile = open("csgo_results.csv",'w')
+            csvFile.write("Winning Team, Losing Team, Winning Score, Losing Score, Map Played")
+        if i>0:
+            hltvUrl+=(str((i)*50)+'/')
+        print(hltvUrl)
+        r  = requests.get(hltvUrl)
+        data = r.text
+        soup = BeautifulSoup(data,"lxml")
+        results=soup.find_all("div", {"class": "matchListBox"},limit=50)
+        for j in results:
+            result=(str(j.text).splitlines())
+            csv+=extractDataIntoCondensedList(result)
+    csvFile.write(csv)
 
-
-    #find team ranking on hltv
-def getTeamRank(team):
-    r  = requests.get("http://www.hltv.org/ranking/teams/")
-    data = r.text
-    soup = BeautifulSoup(data)
-    div=soup.div
-    logo = div.attrs['ranking-logo']
-    
-getTeamRank('Natus Vincere')
-
-"""
+scrape(100)
