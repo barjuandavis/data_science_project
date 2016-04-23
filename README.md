1778abf73b68c8d30ae867e50ba4fcb4445ebec3
Rishi <rishijavia@lclark.edu>
2016-04-23 13:10:42 -0700

    trying out new code

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 CS 298 Data Science Project about CS GO data
 
 Teams: fnatic and NIP
-Variables: map, %win on that map, average player rating
+Variables: map, %win on that map, pistol round win %, average player rating
+maps: dust2, mirage, cache, train, inferno
--- project.py
+++ project.py
@@ -7,22 +7,17 @@ Created on Wed Apr 20 12:06:25 2016
 from bs4 import BeautifulSoup
 import requests
 
-hltvUrl = "www.hltv.org/?pageid=179&teamid=4991"
+hltvUrl = "www.hltv.org/?pageid=192&teamid=4991&mapid=31"
 r  = requests.get("http://" +hltvUrl)
 data = r.text
 soup = BeautifulSoup(data, "lxml")
-results=soup.find_all("div", {"class": "matchListBox"},limit=50)
-print(soup.prettify())
+#print(soup.prettify())
 
 print('******************')
-res=results[0]
-print(res.contents)
-
-print(len(res))
-
-for i in res:
-    print(i)
     
-#mydivs = soup.findAll("div", { "class" : "matchScoreCell" })
-#
-#print(mydivs)
+mydivs = soup.findAll("div", { "class" : "covSmallHeadline"})
+
+
+for i in range(len(mydivs)):
+    if "\%" in i:
+        print(i)
