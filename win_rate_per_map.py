52140a9499d28550caada4c65f7a962d0b6ab38b
Rishi <rishijavia@lclark.edu>
2016-04-24 18:18:16 -0700

    More Variables Added

--- win_rate_per_map.py
+++ win_rate_per_map.py
@@ -22,7 +22,7 @@ top_20 = [
 {'name':'Liquid', 'id':5973},
 {'name':'CLG', 'id':5974},
 {'name':'GODSENT', 'id':6902},
-{'name':'G2', 'id':5595},
+{'name':'G2', 'id':5995},
 {'name':'Tempo Storm', 'id':6118},
 {'name':'TyLoo', 'id':4863},
 {'name':'Cloud9', 'id':5752},
@@ -42,15 +42,17 @@ maps=[
 {'name':'overpass', 'id':40},
 ]
 
+
 def scrape():
     csv = ''
     csvFile = open("map_data.csv",'w')
-    csvFile.write("Team, Map, Win %, Pistol Rounds Win %")
+    csvFile.write("Team, Map, Win %, Pistol Rounds Win %, Round win % after getting first kill, Round win % after receiving first death")
     for i in range(len(top_20)):
         team_id = top_20[i]['id']
         for j in range(len(maps)):
             map_id = maps[j]['id']
             hltvUrl = "http://www.hltv.org/?pageid=192&teamid=%d&mapid=%d" % (team_id, map_id)
+            hltvUrl2 = "http://www.hltv.org/?pageid=192&teamid=%d&mapid=%d" % (team_id, map_id)
             print("Hitting: " + hltvUrl)
             r  = requests.get(hltvUrl)
             data = r.text
@@ -64,7 +66,20 @@ def scrape():
                 win_in_map = '0'
             if (pistol_in_map == 'N/'):
                 pistol_in_map = '0'
-            line = '\n' + top_20[i]['name'] + ',' + maps[j]['name'] + ',' + win_in_map + ',' + pistol_in_map
+                
+            s  = requests.get(hltvUrl2)
+            data2 = s.text
+            soup2 = BeautifulSoup(data2,"lxml")
+            results2=soup2.find_all("div", {"class": "covMainBoxContent"},limit=1)
+            for k in results2:
+                result2=(str(k.text).splitlines())
+            first_kill = result2[11][42:-2]
+            first_death = result2[18][45:-2]
+            if (first_kill == 'N'):
+                first_kill = '0'
+            if (first_death == 'N'):
+                first_death = '0'
+            line = '\n' + top_20[i]['name'] + ',' + maps[j]['name'] + ',' + win_in_map + ',' + pistol_in_map + ',' + first_kill + ','+first_death
             csv += line
 
     csvFile.write(csv)
